06d31cc6583c66c5c097d051904781b8748af479
Ni7r0g3n <rambaldifabio1@gmail.com>
2024-02-08 06:25:23 +0100

    v2 Fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,25 @@
 import CircleType  from "../CircleType/CircleType";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import Spinner from "../Spinner/Spinner";
 import "./Home.css";
 import Loader from "../Loader/Loader";
 
 function Home({onClick, onImageLoad, markers, loading, imageRef}) {
     
+
+    const Image = useCallback(() => {
+        console.log("image rendered")
+        return (
+            <>
+            <h2 id="siteTitle" className="fadeInDown title" style={{position:"absolute", top: 0, left: 0, backdropFilter: "blur(10px)", padding: "1rem",color:"white", fontFamily: "'Poppins', sans-serif"}}>{"V. P. Romano 23-30"}</h2>
+            <img ref={imageRef} onLoad={onImageLoad} className={`${loading ? null : "fadeIn"} main-image`} style={{maxHeight: "100vh", maxWidth: "100vw", zIndex:98, visibility: loading ? "hidden" : "visible"}} src={"/residence-ar/Image65.jpg"}/>
+            </>
+        )
+    }, [loading])
+
     return (
         <div style={{height: "100vh", display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center"}}>
-            {loading ? null : <h2 id="siteTitle" className="fadeInDown title" style={{position:"absolute", top: 0, left: 0, backdropFilter: "blur(10px)", padding: "1rem",color:"white", fontFamily: "'Poppins', sans-serif"}}>{"V. P. Romano 23-30"}</h2>}
-            <img ref={imageRef} onLoad={onImageLoad} className={`${loading ? null : "fadeIn"} main-image`} style={{maxHeight: "100vh", maxWidth: "100vw", zIndex:98, visibility: loading ? "hidden" : "visible"}} src={"/residence-ar/Image65.png"} onClick={onClick}/>
+            <Image/>
             {markers}
             {loading ? <Loader/> : null}
         </div>
